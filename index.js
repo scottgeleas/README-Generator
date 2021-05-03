f3a4399d119cae210471857cf2e58cc42c1ea6f8
Scott Geleas <scottgeleas97@gmail.com>
2021-05-03 13:03:35 -0400

    added license badges, fixed format, added email and github sections

--- index.js
+++ index.js
@@ -24,46 +24,83 @@ inquirer
             name: 'usage'
         },
         {
-            type: 'list',
-            message: 'License: ',
-            name: 'license',
-            choices: ['MIT', 'Apache 2.0']
-        },
-        {
-            type: 'input',
-            message: 'Contributions: ',
-            name: 'contributions'
-        },
-        {
             type: 'input',
             message: 'Tests: ',
             name: 'tests'
         },
         {
             type: 'input',
-            message: 'FAQs: ',
-            name: 'faqs'
+            message: 'Contributing: ',
+            name: 'contributing'
+        },
+        {
+            type: 'list',
+            message: 'License: ',
+            name: 'license',
+            choices: ['Apache 2.0', 'MIT', "Mozilla Public License 2.0", "WTFPL", "Unlicense"]
+        },
+        {
+            type: 'input',
+            message: 'Github/ ',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'Email: ',
+            name: 'email'
         }
+        
     ])
     .then((response) => {
         console.log(response)
+
+        let licenseBadge = "";
+        switch (response.license) {
+            case "MIT":
+                licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" 
+            break;
+            case "Apache 2.0":
+                licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+            break;
+            case "Unlicense":
+                licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+            break;
+            case "Mozilla Public License 2.0":
+                licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+            break;
+            case "WTFPL":
+                licenseBadge = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)"
+            break;
+        };
+
         fs.writeFile('README.md',
         `# ${response.title}
-        ## Description 
-        ${response.description}
-        ## Installation
-        ${response.install}
-        ## Usage
-        ${response.usage}
-        ## License
-        ${response.license}
-        ## Contributions
-        ${response.contributions}
-        ## Tests
-        ${response.tests}
-        ### Frequently Asked Questions
-        ${response.faqs}`,
-        info =>
-        console.log('README creation successful')
-        )
-    });
+${licenseBadge}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [License](#license)
+- [Questions](#questions)
+## Description
+${response.description}
+## Installation
+${response.install}
+## Usage
+${response.usage}
+## Tests
+${response.tests}
+## Contributing
+${response.contributing}
+## License
+${response.license}
+### Questions
+- [Github](https://github.com/${response.github})
+- <${response.email}>`,
+() => { 
+    console.log('README creation successful')
+}
+)}
+)
--- sample.md
+++ sample.md
@@ -1,25 +1,22 @@
-# 
+#hello world
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
-- [Tests](#tests)
-- [Contributing](#contributing)
 - [License](#license)
-- [Questions](#questions)
+- [Tests](#tests)
 ## Description
 
 ## Installation
-
+instructions on how to setup so user can run it
 ## Usage
-
-## Tests
-
-## Contributing
-
+making readme generators 
 ## License
 Apache 2.0
+## Contributing
+instuctions on how to contribute
+## Tests
+instruction on how to test
+npm run (test)
 ### Questions
-- [Github](https://github.com/scottgeleas)
-- <scottgeleas97@gmail.com>
