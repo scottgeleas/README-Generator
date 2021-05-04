fe03af36f1337f426a1f7197f1328a5b56ce3236
Scott Geleas <scottgeleas97@gmail.com>
2021-05-04 08:12:53 -0400

    added license descirption, fixed formatting, added license-info.js

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,6 @@
 let inquirer = require("inquirer");
 let fs = require("fs");
-// Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+let licenseInfo = require("./license-info");
 inquirer
     .prompt([
         {
@@ -25,19 +25,18 @@ inquirer
         },
         {
             type: 'input',
+            message: 'Contributing: ',
+            name: 'contributing'
+        },
+        {
+            type: 'input',
             message: 'Tests: ',
             name: 'tests'
         },
         {
             type: 'input',
-            message: 'Contributing: ',
-            name: 'contributing'
-        },
-        {
-            type: 'list',
-            message: 'License: ',
-            name: 'license',
-            choices: ['Apache 2.0', 'MIT', "Mozilla Public License 2.0", "WTFPL", "Unlicense"]
+            message: 'Questions:',
+            name: 'questions'
         },
         {
             type: 'input',
@@ -48,59 +47,73 @@ inquirer
             type: 'input',
             message: 'Email: ',
             name: 'email'
+        },
+        {
+            type: 'list',
+            message: 'License: ',
+            name: 'license',
+            choices: ['Apache 2.0', 'MIT', "Mozilla Public License 2.0", "WTFPL", "Unlicense"]
         }
-        
     ])
     .then((response) => {
         console.log(response)
-
         let licenseBadge = "";
+        let licenseDesc = "";
         switch (response.license) {
             case "MIT":
-                licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" 
-            break;
-            case "Apache 2.0":
-                licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-            break;
-            case "Unlicense":
-                licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
-            break;
-            case "Mozilla Public License 2.0":
-                licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-            break;
-            case "WTFPL":
-                licenseBadge = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)"
-            break;
-        };
+                licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+                licenseDesc = mit;
+                break;
 
+            case "Apache 2.0":
+                licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+                licenseDesc = apache;
+                break;
+
+            case "Unlicense":
+                licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+                licenseDesc = unlicense;
+                break;
+
+            case "Mozilla Public License 2.0":
+                licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+                licenseDesc = mpl2;
+                break;
+
+            case "WTFPL":
+                licenseBadge = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+                licenseDesc = wtfpl;
+                break;
+        };
         fs.writeFile('README.md',
-        `# ${response.title}
+            `# ${response.title}
 ${licenseBadge}
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
-- [Tests](#tests)
 - [Contributing](#contributing)
-- [License](#license)
+- [Tests](#tests)
 - [Questions](#questions)
+- [License](#license)
 ## Description
 ${response.description}
 ## Installation
 ${response.install}
 ## Usage
 ${response.usage}
-## Tests
-${response.tests}
 ## Contributing
 ${response.contributing}
-## License
-${response.license}
+## Tests
+${response.tests}
 ### Questions
+${response.questions}
 - [Github](https://github.com/${response.github})
-- <${response.email}>`,
-() => { 
-    console.log('README creation successful')
-}
-)}
-)
+- <${response.email}>
+## License
+${licenseDesc}`,
+            () => {
+                console.log('README creation successful')
+            }
+        )
+    });
